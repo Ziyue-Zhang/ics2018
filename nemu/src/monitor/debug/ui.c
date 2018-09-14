1136a1d9679cdc83cd417fd470492fb053659226
tracer-ics2018 <tracer@njuics.org>
2018-09-14 15:35:09 +0800

    >  compile 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:35:09 up 6 min,  1 user,  load average: 0.08, 0.35, 0.24 3df144456f041719fda887dd9fd2f28cec9feee

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -158,8 +158,8 @@ static int cmd_x(char *args) {
       int n = atoi(num);
       if (arg[0] == '0' && arg[1] == 'x')
 	{
+	printf("%d",n);	
 	int *address = (int *) htoi(arg);
-	printf("%d",n);
 	for (int i = 0; i < n; i++, address++)
           printf("0x%p: %08x\n", address, *address);
 	}
