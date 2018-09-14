c3d55aa574a526ca6944a4f0a821972728cbe2a4
tracer-ics2018 <tracer@njuics.org>
2018-09-14 11:40:12 +0800

    >  compile 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:40:12 up  1:15,  1 user,  load average: 0.38, 0.22, 0.11 f3ab2fe03d42ce665c90b504a46c6f2c5dbb6e8a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -102,10 +102,10 @@ static int cmd_info(char *args) {
   char *arg = strtok(NULL, " ");
 
   if (arg[0] == 'r') {
-     printf("$eax = 0x%8x		$edx = 0x%8x\n", cpu.eax,cpu.edx);
-     printf("$ecx = 0x%8x		$ebx = 0x%8x\n", cpu.ecx,cpu.ebx);
-     printf("$ebp = 0x%8x		$esi = 0x%8x\n", cpu.ebp,cpu.esi);
-     printf("$edi = 0x%8x		$esp = 0x%8x\n", cpu.edi,cpu.esp);
+     printf("$eax = 0x%x		$edx = 0x%x\n", cpu.eax,cpu.edx);
+     printf("$ecx = 0x%x		$ebx = 0x%x\n", cpu.ecx,cpu.ebx);
+     printf("$ebp = 0x%x		$esi = 0x%x\n", cpu.ebp,cpu.esi);
+     printf("$edi = 0x%x		$esp = 0x%x\n", cpu.edi,cpu.esp);
     }
   else if (arg[0] == 'w') {
 
