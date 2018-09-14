6bfaf95270beaf0d312db80a35fb7a3be0eb32e4
tracer-ics2018 <tracer@njuics.org>
2018-09-14 14:32:23 +0800

    >  compile 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:32:23 up 4 min,  1 user,  load average: 0.01, 0.03, 0.00 8e0f55a9738eb35e68021ae9bf27ba27d670480c

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,10 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args);
+
+static int cmd_info(char *args);
+
 static struct {
   char *name;
   char *description;
@@ -46,7 +50,8 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si [N]", "Step one instruction exactly", cmd_si},
+  { "info SUBCMD", "Generic command for showing things about the program being debugged", cmd_info}, 
   /* TODO: Add more commands */
 
 };
@@ -73,6 +78,38 @@ static int cmd_help(char *args) {
     }
     printf("Unknown command '%s'\n", arg);
   }
+  return 0;
+}
+
+
+static int cmd_si(char *args) {
+  char *arg = strtok(NULL, " ");
+  
+  if (arg == NULL) {
+    cpu_exec(1);
+  }
+  else {
+   int num = atoi(arg);
+   cpu_exec(num);
+   return 0;
+  }
+  return 0;
+}
+
+static int cmd_info(char *args) {
+  char *arg = strtok(NULL, " ");
+
+  if (arg[0] == 'r') {
+     printf("$eax = 0x%x		$edx = 0x%x\n", cpu.eax,cpu.edx);
+     printf("$ecx = 0x%x		$ebx = 0x%x\n", cpu.ecx,cpu.ebx);
+     printf("$ebp = 0x%x		$esi = 0x%x\n", cpu.ebp,cpu.esi);
+     printf("$edi = 0x%x		$esp = 0x%x\n", cpu.edi,cpu.esp);
+    }
+  else if (arg[0] == 'w') {
+
+    }
+  else
+    printf("Unknown command '%s'\n", arg);
   return 0;
 }
 
