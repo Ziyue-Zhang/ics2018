cef3b0e1a02f87af00f57267de49f376647682a7
tracer-ics2018 <tracer@njuics.org>
2018-09-26 08:36:15 +0800

    >  compile 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  08:36:15 up 32 min,  1 user,  load average: 0.55, 0.27, 0.16 44d8abd9764d3f72907a3547c5b7a19dbb378c0c

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -1,6 +1,7 @@
 #include "nemu.h"
 #include "monitor/monitor.h"
 #include "monitor/watchpoint.h"
+#include "monitor/expr.h"
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
  * This is useful when you use the `si' command.
@@ -44,10 +45,10 @@ void cpu_exec(uint64_t n) {
 	if (!head)
 	{
 		WP *p = head;
-		while(p->next)
+		while(p)
 		{
 			bool flag = true;
-			uint32_t new_result = p->next->result;
+			uint32_t new_result = expr(p->expr,&flag);
 			if(!flag)
 				assert(0);
 			else
