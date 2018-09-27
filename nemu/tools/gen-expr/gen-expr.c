2da43591e630e9a16db012bcb371902c3f363fd4
tracer-ics2018 <tracer@njuics.org>
2018-09-27 16:25:34 +0800

    >  compile 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:25:34 up 19 min,  1 user,  load average: 0.52, 0.18, 0.12 f76f8ac759475d6e6df30ca7f0633c3a2f4356a4

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -10,14 +10,66 @@ static char buf[65536];
 static inline void gen_rand_expr() {
   buf[0] = '\0';
 }
-/*void gen_rand_expr() {
-	switch (choose(3)) {
-		case 0: gen_num(); break;
-		case 1: gen('('); gen_rand_expr(); gen(')'); break;	
-		default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+void gen_rand_expr(unsigned int head, unsigned int tail)
+{
+	if (tail - head >= 2)
+	{
+		switch (choose(10))
+		{
+		case 0:
+		case 1:
+		{
+			if (tail - head >= 2)
+				gen_rand_expr(head, tail);
+			else
+			{
+				if (head == 0 && head == tail || head > 0 && buf[head - 1] > '0' && buf[head - 1] < '9')
+					for (unsigned int i = head; i <= tail; i++)
+						gen_num(i);
+				else
+				{
+					buf[head] = choose(9) + 1 + '0';
+					for (unsigned int i = head + 1; i <= tail; i++)
+						gen_num(i);
+				}
+			}
+		}
+		break;
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		{
+			unsigned int b = choose(tail - head) + head;
+			if (b == head)
+				b++;
+			else if (b == tail)
+				b--;
+			gen_rand_op(b);
+			gen_rand_expr(head, b - 1);
+			gen_rand_expr(b + 1, tail);
+		}
+		break;
+		default:
+			buf[head] = '('; gen_rand_expr(head + 1, tail - 1); buf[tail] = ')';
+		break;
+		}
 	}
-}*/
-
+	else
+	{
+		if (head == 0 && head == tail || head>0 && buf[head - 1] > '0' && buf[head - 1] < '9')
+			for (unsigned int i = head; i <= tail; i++)
+				gen_num(i);
+		else
+		{
+			buf[head] = choose(9) + 1 + '0';
+			for (unsigned int i = head + 1; i <= tail; i++)
+				gen_num(i);
+		}
+	}
+}
 static char code_buf[65536];
 static char *code_format =
 "#include <stdio.h>\n"
