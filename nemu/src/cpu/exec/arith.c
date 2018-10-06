28209046a8617cc4c7fb2486e3d749ffe41c3e6b
tracer-ics2018 <tracer@njuics.org>
2018-10-06 12:48:52 +0800

    >  compile 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  12:48:52 up  1:31,  1 user,  load average: 0.12, 0.05, 0.06 ed0f504d26d9ece07ef86a6e1d4def7ef8566dbd

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -14,7 +14,7 @@ make_EHelper(sub) {
   rtl_update_ZFSF(&t2, id_dest->width);
 
   //rtl_setrelop(RELOP_LTU, &t0, &id_dest->val, &t2);
-  //rtl_or(&t0, &t3, &t0);
+  rtl_or(&t0, &t3, &t0);
   rtl_set_CF(&t0);
 
   rtl_xor(&t0, &id_dest->val, &id_src->val);
