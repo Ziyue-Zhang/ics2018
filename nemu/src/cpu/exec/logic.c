e6ff812e3f5a21d621cb20b6e2cb3f1d51e45c92
tracer-ics2018 <tracer@njuics.org>
2018-10-06 12:47:01 +0800

    >  compile 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  12:47:01 up  1:29,  1 user,  load average: 0.03, 0.04, 0.06 a5373f756cb29fd1bdeebbc0b5b93f47517aa876

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -1,5 +1,6 @@
 #include "cpu/exec.h"
 #include "cpu/cc.h"
+const rtlreg_t tzero = 0;
 
 make_EHelper(test) {
   TODO();
@@ -14,13 +15,25 @@ make_EHelper(and) {
 }
 
 make_EHelper(xor) {
-  TODO();
+  //TODO();
+  rtl_xor(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+  
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
+  rtl_update_ZFSF(&t2, id_dest->width);
 
   print_asm_template2(xor);
 }
 
 make_EHelper(or) {
-  TODO();
+  //TODO();
+  rtl_or(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
+  rtl_update_ZFSF(&t2, id_dest->width);
 
   print_asm_template2(or);
 }
