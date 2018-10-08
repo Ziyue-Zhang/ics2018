22af5ee9673a4db376474db679859b84afb3dabe
tracer-ics2018 <tracer@njuics.org>
2018-10-08 10:42:31 +0800

    >  compile 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:42:31 up  2:39,  1 user,  load average: 0.00, 0.01, 0.00 ee54ff0b6425d40e0929f582b222448839dc2e

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -11,7 +11,6 @@ make_EHelper(jmp) {
 make_EHelper(jcc) {
   // the target address is calculated at the decode stage
   uint32_t cc = decoding.opcode & 0xf;
-  printf("xjajshgdjahgsjdhgasdm\n");
   rtl_setcc(&t0, cc);
   rtl_li(&t1, 0);
   rtl_jrelop(RELOP_NE, &t0, &t1, decoding.jmp_eip);
