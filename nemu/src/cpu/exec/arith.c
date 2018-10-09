35f1226fc4009e41a4d51f13bf84370434049b21
tracer-ics2018 <tracer@njuics.org>
2018-10-09 11:37:54 +0800

    >  compile 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:37:54 up  3:30,  1 user,  load average: 0.07, 0.10, 0.04 cf499c754575aac422014744a407b5993a055c29

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -10,6 +10,7 @@ make_EHelper(add) {
   rtl_setrelop(RELOP_LTU, &t0, &t2, &id_dest->val);
   rtl_or(&t0, &t3, &t0);
   rtl_set_CF(&t0);
+  printf("%d\n",cpu.eflags.CF);
 
   rtl_xor(&t0, &id_dest->val, &id_src->val);
   rtl_not(&t0, &t0);
