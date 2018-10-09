ba3d34d6d30d2dd45e9f9cb1aef722c23d37e99c
tracer-ics2018 <tracer@njuics.org>
2018-10-09 19:23:51 +0800

    >  compile 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:23:51 up  6:56,  1 user,  load average: 0.13, 0.12, 0.12 2cc5d90611f22b38856814bf2768d8801c1bf

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -33,7 +33,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
 		*dest = (cpu.eflags.SF != cpu.eflags.OF); 
 		break;
 	case CC_LE:
-		*dest = (cpu.eflags.ZF) && (cpu.eflags.SF != cpu.eflags.OF);
+		*dest = (cpu.eflags.ZF) || (cpu.eflags.SF != cpu.eflags.OF);
 		break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
