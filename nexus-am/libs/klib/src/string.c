ff3c39a71c5c86a2cfec2dfef6e44ee24427a3b4
tracer-ics2018 <tracer@njuics.org>
2018-10-10 19:31:09 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:31:09 up  9:40,  1 user,  load average: 0.21, 0.12, 0.04 8d96847960847d995afff9f41f254b9a50d14d29

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -45,7 +45,7 @@ char* strcat(char* dst, const char* src) {
 
 int strcmp(const char* s1, const char* s2) {
   assert(s1 && s2);
-  while (*s1 == *s2 && *s1 && *s2)
+  while (*s1 == *s2 && *s1)
   {
       s1++;
       s2++;
@@ -59,9 +59,9 @@ int strcmp(const char* s1, const char* s2) {
   return 0;
 }
 
-/*int strncmp(const char* s1, const char* s2, size_t n) {
+int strncmp(const char* s1, const char* s2, size_t n) {
   assert(s1 && s2);
-  while (n > 0 && *s1 && *s2) {
+  while (n > 0 && *s1) {
 	  if (*s1 != *s2) {
 		  int ret = *(unsigned char*)s1 - *(unsigned char*)s2;
 		  if(ret > 0)
@@ -74,7 +74,7 @@ int strcmp(const char* s1, const char* s2) {
 	  n--;
   }
   return 0;
-}*/
+}
 
 void* memset(void* v,int c,size_t n) {
   char* temp = v;
