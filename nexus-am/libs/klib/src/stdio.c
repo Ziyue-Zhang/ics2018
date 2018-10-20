b5c78662cb57762e533b7caa8b97d9f63901a2b9
tracer-ics2018 <tracer@njuics.org>
2018-10-20 17:20:14 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:20:14 up  2:51,  1 user,  load average: 0.04, 0.03, 0.00 c68ca2db4b5a25d67f690d0b3f25b0f25845fe0d

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -5,14 +5,22 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  return 0;
+  /*assert(fmt);
+  va_list va;
+  int n = 0;
+  va_start(va, fmt);
+  while(*fmt != '\0')
+  {
+
+  }*/
+	return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   return 0;
 }
 
-int sprintf(char *out, const char *fmt, ...) {
+/*int sprintf(char *out, const char *fmt, ...) {
   assert(fmt);
   va_list va;
   int n = 0;
@@ -50,6 +58,61 @@ int sprintf(char *out, const char *fmt, ...) {
 				out++;
 				n++;
 			}
+		}
+	}
+	else {
+		*out = *fmt;
+		out++;
+		n++;
+	}
+	fmt++;
+  }
+  *out = '\0';
+  va_end(va);
+  return n;
+}*/
+
+int sprintf(char *out, const char *fmt, ...) {
+  assert(fmt);
+  va_list va;
+  int n = 0;
+  va_start(va, fmt);
+  while(*fmt != '\0')
+  {
+	if (*fmt == '%') {
+		switch (*(fmt + 1)) {
+		case 's':{
+			fmt++;
+			char *s = va_arg(va, char*);
+			while (*s != '\0')
+			{
+				*out = *s;
+				out++;
+				n++;
+				s++;
+			}
+		}
+		case 'd':{
+			fmt++;
+			int result = va_arg(va, int);
+			char d[32];
+			int temp = result;
+			int i;
+			for (i = 0; temp != 0; i++)
+			{
+				d[i] = temp % 10 + '0';
+				temp /= 10;
+			}
+			i--;
+			while (i >= 0)
+			{
+				*out = d[i];
+				i--;
+				out++;
+				n++;
+			}
+		}
+		default:break;
 		}
 	}
 	else {
