baa55e9ddccb777aa495d136ac21f0c030334e4a
tracer-ics2018 <tracer@njuics.org>
2018-10-24 18:49:33 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  18:49:33 up 18 min,  1 user,  load average: 0.03, 0.08, 0.11 1245e9d3722bf914c8d17255493d63e7afa50e09

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -1,15 +1,18 @@
 #include <am.h>
 #include <x86.h>
 #include <amdev.h>
-#define KEYDOWN_MASK 0x8000
+//#define KEYDOWN_MASK 0x8000
 
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
 	  uint32_t k = inw(0x60);
-      kbd->keydown = (k & KEYDOWN_MASK ? 1 : 0);
-	  kbd->keycode = k & ~KEYDOWN_MASK;
+	  uint32_t s = inb(0x64);
+      /*kbd->keydown = (k & KEYDOWN_MASK ? 1 : 0);
+	  kbd->keycode = k & ~KEYDOWN_MASK;*/
+      kbd->keydown = s;
+	  kbd->keycode = k;
       return sizeof(_KbdReg);
     }
   }
