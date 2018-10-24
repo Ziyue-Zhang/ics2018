b2ebaa626dbdc09fc6b2ea772dbaf7ba06fd418c
tracer-ics2018 <tracer@njuics.org>
2018-10-24 11:11:28 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:11:28 up 42 min,  1 user,  load average: 0.27, 0.15, 0.16 89931c522051e7993e922834cfbbc84cd877355

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -4,7 +4,13 @@
 #include <klib.h>
 
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
-
+int min(int a, int b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+}
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
@@ -21,11 +27,18 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-
-	  int i;
-	  int size = screen_width() * screen_height();
-	  for (i = 0; i < size; i ++) fb[i] = i;
-
+      
+	  //int i;
+	  //int size = screen_width() * screen_height();
+	  //for (i = 0; i < size; i ++) fb[i] = i;
+      
+	  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h, W = 400, H = 300;
+	  uint32_t *pixels = ctl->pixels;
+	  int cp_bytes = sizeof(uint32_t) * min(w, W - x);
+	  for (int j = 0; j < h && y + j < H; j ++) {
+	    memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+	    pixels += w;
+	  }
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
