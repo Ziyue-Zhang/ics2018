f6f8bf28e4bf1ad52ba30dcf88acbb5dbc328154
tracer-ics2018 <tracer@njuics.org>
2018-11-01 11:17:59 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:17:59 up  1:10,  1 user,  load average: 0.16, 0.45, 0.48 f57d1cd68e7bf7579bfd39fac2f5161b88c92cc3

--- nexus-am/am/arch/x86-nemu/src/devices/timer.c
+++ nexus-am/am/arch/x86-nemu/src/devices/timer.c
@@ -3,7 +3,7 @@
 #include <amdev.h>
 #define RTC 0x48
 extern uint32_t uptime();
-//static uint32_t boot;
+static uint32_t boot;
 
 size_t timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
@@ -11,10 +11,10 @@ size_t timer_read(uintptr_t reg, void *buf, size_t size) {
       uint32_t now;
 	  now = inl(RTC);
 	  _UptimeReg *uptime = (_UptimeReg *)buf;
-	  long useconds = now;
+	  long useconds = now - boot;
       uptime->hi = 0;
-      uptime->lo = (useconds + 500) / 1000;
-      //uptime->lo = inl(RTC);
+      //uptime->lo = (useconds + 500) / 1000;
+      uptime->lo = useconds;
 	  return sizeof(_UptimeReg);
     }
     case _DEVREG_TIMER_DATE: {
@@ -32,5 +32,5 @@ size_t timer_read(uintptr_t reg, void *buf, size_t size) {
 }
 
 void timer_init() {
-	//boot = inl(RTC);
+	boot = inl(RTC);
 }
