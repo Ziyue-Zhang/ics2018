f67146ec1c6e0f64d18e2cf72f57aca91f2d3bf4
tracer-ics2018 <tracer@njuics.org>
2018-11-01 10:57:39 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:57:39 up 50 min,  1 user,  load average: 0.93, 0.58, 0.35 781af2a7b75fcd759c753b68d99b6af94e1a8d61

--- nexus-am/tests/keytest/main.cpp
+++ nexus-am/tests/keytest/main.cpp
@@ -16,9 +16,10 @@ int main() {
     int key = read_key();
     bool down = false;
     if (key & 0x8000) {
+	  printf("key1=%d\n",key);
       key ^= 0x8000;
       down = true;
-	  printf("xjhhkdsfhj\n");
+	  printf("key2=%d\n",key);
     }
     if (key != _KEY_NONE) {
       printf("Get key: %d %s %s\n", key, names[key], down ? "down" : "up");
