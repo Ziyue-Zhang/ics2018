c8e035754bb7bff3c1066fae6b3af9052bd73e0a
tracer-ics2018 <tracer@njuics.org>
2018-11-01 10:49:34 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:49:34 up 42 min,  1 user,  load average: 0.63, 0.39, 0.24 11ab60a083028389fbafbd12e55e4dd09adb82a6

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -7,15 +7,13 @@ size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
-	  int32_t k = inl(0x60);
-	  //uint32_t s = inb(0x64);
+	  uint32_t k = inl(0x60);
+	  uint32_t s = inb(0x64);
       /*kbd->keydown = (k & KEYDOWN_MASK ? 1 : 0);
 	  kbd->keycode = k & ~KEYDOWN_MASK;*/
 	  //printf("%d\n",s);
-	  kbd->keydown = ((k >> 15) & 1);
-	  kbd->keycode = k & 0x7fff;
-      //kbd->keydown = s;
-	  //kbd->keycode = k;
+      kbd->keydown = s;
+	  kbd->keycode = k;
 	  //if(s!=0)
 		//  kbd->keycode = _KEY_NONE;
       return sizeof(_KbdReg);
