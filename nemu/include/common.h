d0375b819714a675ef551dd01a3ae91d835461af
tracer-ics2018 <tracer@njuics.org>
2018-11-04 15:33:36 +0800

    >  compile 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:33:36 up  4:36,  1 user,  load average: 0.12, 0.03, 0.01 9f30d37d364828a3bb6657bb3aa8926eb1c470b7

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -1,9 +1,9 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define HAS_IOE
-//#define DEBUG
-//#define DIFF_TEST
+//#define HAS_IOE
+#define DEBUG
+#define DIFF_TEST
 
 #if _SHARE
 // do not enable these features while building a reference design
