3076a3bcf30ae3506e9cc86f178aba580b6cce97
tracer-ics2018 <tracer@njuics.org>
2018-11-24 15:34:56 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:34:56 up  4:35,  1 user,  load average: 0.06, 0.08, 0.08 4baed2147537bfe8174592fef94d3670c76c98ca

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,7 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
+	case _EVENT_SYSCALL: Log("NMSL"); break;
 	case _EVENT_YIELD: Log("nmsl"); break;
 	default: panic("Unhandled event ID = %d", e.event);
   }
