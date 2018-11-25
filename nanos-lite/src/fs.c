4db7c3fb829cbce77faf142573284613735908b9
tracer-ics2018 <tracer@njuics.org>
2018-11-25 13:55:34 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  13:55:34 up 17:21,  1 user,  load average: 0.09, 0.07, 0.02 5462969be5be29e824d72e188176d74182deb27

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -36,3 +36,4 @@ static Finfo file_table[] __attribute__((used)) = {
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
+
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,23 @@
 #include "common.h"
 #include "syscall.h"
 
+extern void _putc(char ch);
+
+uint32_t syswrite(int fd, const void *buf, size_t count)
+{
+	uint32_t len = 0;
+	if(fd == 1 || fd == 2)
+	{
+		while(count)
+		{
+			count--;
+			_putc(((char*)buf)[len]);
+			len++;
+		}
+	}
+	return len;
+}
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -10,7 +27,7 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
 	case SYS_yield: _yield(), c->GPRx = 0; break;
 	case SYS_exit: _halt(a[0]); break;
-    case SYS_write: Log("nmsl"); break;
+    case SYS_write: c->GPRx = syswrite(a[1], (void *)a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
