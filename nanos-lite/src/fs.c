ba81d71608f7fb2f60e17faf2efa97fe2f288929
tracer-ics2018 <tracer@njuics.org>
2018-11-27 17:20:07 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:20:07 up  6:16,  1 user,  load average: 0.05, 0.01, 0.00 ce5b899c7c0d8d487737898451a81f386d7b7900

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -7,7 +7,7 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
-  //off_t open_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -26,9 +26,9 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin", 0, 0, invalid_read, invalid_write},
-  {"stdout", 0, 0, invalid_read, invalid_write},
-  {"stderr", 0, 0, invalid_read, invalid_write},
+  {"stdin", 0, 0, 0, invalid_read, invalid_write},
+  {"stdout", 0, 0, 0, invalid_read, invalid_write},
+  {"stderr", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
@@ -38,3 +38,7 @@ void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
 
+int fs_open(const char *pathname, int flags, int mode)
+{
+	return 0;
+}
