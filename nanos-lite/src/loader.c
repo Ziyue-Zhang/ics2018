14ecfcfbd870a59235129f443872a0b7a98e3ffc
tracer-ics2018 <tracer@njuics.org>
2018-11-27 20:55:05 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:55:05 up  9:51,  1 user,  load average: 0.06, 0.17, 0.09 1816c653d8e22e81a4835ef1a4d6a4b0efb72611

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,6 +10,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
+  Log("nmsl");
   uintptr_t entry = loader(pcb, filename);
   ((void(*)())entry) ();
 }
