eb70f6349cadecb77c56d0823da7c5e1a0d728be
tracer-ics2018 <tracer@njuics.org>
2018-11-27 20:42:25 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:42:25 up  9:38,  1 user,  load average: 0.27, 0.10, 0.03 f3a0a53ea5b5aa9e3e991040c8df172165b4e527

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,13 +1,21 @@
+#include <unistd.h>
 #include "fs.h"
 
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
+int fs_open(const char *pathname, int flags, int mode);
+ssize_t fs_read(int fd, void *buf, size_t len);
+ssize_t fs_write(int fd, const void *buf, size_t len);
+off_t fs_lseek(int fd, off_t offset, int whence);
+int fs_close(int fd);
+size_t fs_filesz(int fd);
+extern void ramdisk_read();
 
 typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
-  size_t open_offset;
+  off_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -47,6 +55,19 @@ int fs_open(const char *pathname, int flags, int mode)
 	}
 	assert(0);
 	return -1;
+}
+
+ssize_t fs_read(int fd, void *buf, size_t len)
+{
+	ssize_t fs_size = fs_filesz(fd);
+	ssize_t fs_offset = file_table[fd].open_offset;
+	if(fs_offset >= fs_size || len == 0)
+		return 0;
+    if(fs_offset + len > fs_size)
+	    len = fs_size - fs_offset;
+	ramdisk_read(buf, fs_offset + file_table[fd].disk_offset, len);
+	file_table[fd].open_offset += len;
+	return len;
 }
 
 size_t fs_filesz(int fd) 
