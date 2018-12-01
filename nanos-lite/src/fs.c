d7d0d6964269046889d0daa0d71edb41f5b248ae
tracer-ics2018 <tracer@njuics.org>
2018-12-02 00:25:05 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:25:05 up  9:58,  1 user,  load average: 0.00, 0.10, 0.21 4a9cf90b5f1afeaef5a05b5681d108778986fa06

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -104,6 +104,7 @@ Log("size:%d offset:%d len:%d",fs_size, fs_offset, len);
 	{		
 		ramdisk_write(buf, fs_offset + file_table[fd].disk_offset, len);
 		file_table[fd].open_offset += len;
+		Log("nmsl");
 		return len;
 	}
 }
