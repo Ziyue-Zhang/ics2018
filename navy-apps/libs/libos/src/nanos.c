a20729f109c5d4b2bff6d6ac5ae418a2529e2fbd
tracer-ics2018 <tracer@njuics.org>
2018-12-01 15:27:53 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:27:53 up  1:14,  1 user,  load average: 0.22, 0.16, 0.10 5b34e63bbc634b30692b3445203e82c4ce2c85e7

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -28,9 +28,9 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  //_exit(SYS_open);
-  //return 0;
-   return _syscall_(SYS_open, (uintptr_t)path, flags, mode);
+  _exit(SYS_open);
+  return 0;
+   //return _syscall_(SYS_open, (uintptr_t)path, flags, mode);
 }
 
 int _write(int fd, void *buf, size_t count){
@@ -66,9 +66,9 @@ int _close(int fd) {
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _exit(SYS_lseek);
-  //return _syscall_(SYS_lseek, fd, offset, whence);
-  return 0;
+  //_exit(SYS_lseek);
+  return _syscall_(SYS_lseek, fd, offset, whence);
+  //return 0;
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
