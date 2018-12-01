05a3faa65eaebfa017a7a3c931e248f83186551f
tracer-ics2018 <tracer@njuics.org>
2018-12-01 15:17:52 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:17:52 up  1:04,  1 user,  load average: 0.21, 0.10, 0.08 9accb97d13ae17b074e07a4018175116f6aba7dc

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -28,14 +28,14 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  _exit(SYS_open);
-  return 0;
+  //_exit(SYS_open);
+  //return 0;
+   return _syscall_(SYS_open, (uintptr_t)path, flags, mode);
 }
 
 int _write(int fd, void *buf, size_t count){
   //_exit(SYS_write);
-   _syscall_(SYS_write, fd, (intptr_t)buf, count);
-  return count;
+   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
 extern end;
@@ -54,20 +54,21 @@ void *_sbrk(intptr_t increment){
 }
 
 int _read(int fd, void *buf, size_t count) {
-  _exit(SYS_read);
-  //_syscall_(SYS_read, fd, (intptr_t)buf, count);
-  return 0;
+  //_exit(SYS_read);
+  return _syscall_(SYS_read, fd, (intptr_t)buf, count);
+  //return 0;
 }
 
 int _close(int fd) {
-  _exit(SYS_close);
-  return 0;
+  //_exit(SYS_close);
+  //return 0;
+  return _syscall_(SYS_close, fd, 0, 0);
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _exit(SYS_lseek);
-  //_syscall_(SYS_lseek, fd, offset, whence);
-  return 0;
+  //_exit(SYS_lseek);
+  _syscall_(SYS_lseek, fd, offset, whence);
+  //return 0;
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
