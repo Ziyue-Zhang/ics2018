0686928c9128d9b944ca1de77d426918b1632237
tracer-ics2018 <tracer@njuics.org>
2018-12-01 22:20:19 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:20:19 up  8:06,  1 user,  load average: 0.13, 0.17, 0.34 41f9c763bc3eaa7aad8e4d95ad168be66d606078

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -68,7 +68,7 @@ ssize_t fs_read(int fd, void *buf, size_t len)
 		return 0;
 	if(fs_offset + len > fs_size)
 	    len = fs_size - fs_offset;
-	Log("size:%d offset:%d len:%d",fs_size, fs_offset, len);
+	//Log("size:%d offset:%d len:%d",fs_size, fs_offset, len);
 	if(file_table[fd].read)
 	{
 		file_table[fd].open_offset += len;
