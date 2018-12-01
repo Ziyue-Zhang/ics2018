2163096a405dc10ee1b702617de4a763e08bb741
tracer-ics2018 <tracer@njuics.org>
2018-12-01 22:05:19 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:05:19 up  7:51,  1 user,  load average: 0.11, 0.26, 0.55 987482f8749c26a85d0e425862dddb4a2cc10296

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -62,16 +62,16 @@ int fs_open(const char *pathname, int flags, int mode)
 
 ssize_t fs_read(int fd, void *buf, size_t len)
 {
+	ssize_t fs_size = fs_filesz(fd);
+	ssize_t fs_offset = file_table[fd].open_offset;	
+	if(fs_offset >= fs_size)
+		return 0;
+	if(fs_offset + len > fs_size)
+	    len = fs_size - fs_offset;
 	if(file_table[fd].read)
 		return file_table[fd].read(buf, file_table[fd].open_offset, len);
 	else
 	{	
-		ssize_t fs_size = fs_filesz(fd);
-		ssize_t fs_offset = file_table[fd].open_offset;
-		if(fs_offset >= fs_size)
-			return 0;
-	    if(fs_offset + len > fs_size)
-		    len = fs_size - fs_offset;
 		ramdisk_read(buf, fs_offset + file_table[fd].disk_offset, len);
 		file_table[fd].open_offset += len;
 		return len;
@@ -80,16 +80,16 @@ ssize_t fs_read(int fd, void *buf, size_t len)
 
 ssize_t fs_write(int fd, const void *buf, size_t len)
 {	
+	ssize_t fs_size = fs_filesz(fd);
+	ssize_t fs_offset = file_table[fd].open_offset;	
+	if(fs_offset >= fs_size)
+		return 0;
+    if(fs_offset + len > fs_size)
+		len = fs_size - fs_offset;	
 	if(file_table[fd].write)
 		return file_table[fd].write(buf, file_table[fd].open_offset, len);
 	else 
 	{
-		ssize_t fs_size = fs_filesz(fd);
-		ssize_t fs_offset = file_table[fd].open_offset;	
-		if(fs_offset >= fs_size)
-			return 0;
-    	if(fs_offset + len > fs_size)
-	   		len = fs_size - fs_offset;
 		ramdisk_write(buf, fs_offset + file_table[fd].disk_offset, len);
 		file_table[fd].open_offset += len;
 		return len;
