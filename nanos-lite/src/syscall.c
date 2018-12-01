348032c497684cf403e5e3fbd04615c9ab916c02
tracer-ics2018 <tracer@njuics.org>
2018-12-02 00:54:42 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:54:42 up 10:27,  1 user,  load average: 0.11, 0.12, 0.13 3245f06f2dbd9a0e4ba7a53a7cc12e142f51f086

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -25,7 +25,7 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
 	case SYS_exit: _halt(a[0]); break;
 	case SYS_yield: _yield(), c->GPRx = 0; break;
-    case SYS_open: c->GPRx = fs_open((char *)a[1], a[2], a[3]); break;
+    case SYS_open: Log("nmsl"), c->GPRx = fs_open((char *)a[1], a[2], a[3]); break;
 	case SYS_read: c->GPRx = fs_read(a[1], (void *)a[2], a[3]); break;
 	case SYS_write: c->GPRx = fs_write(a[1], (void *)a[2], a[3]); break;
 	case SYS_close: c->GPRx = fs_close(a[1]); break;
