7aae312623b8161a39f30fb3df2eb4082c48e6d7
tracer-ics2018 <tracer@njuics.org>
2018-12-01 22:24:53 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:24:53 up  8:11,  1 user,  load average: 0.19, 0.19, 0.29 288fd16de6465a89abbad72698cf8deb02d91e2

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -31,7 +31,6 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  Log("nmsl");
   offset /= sizeof(uint32_t);
   int row = offset / screen_width();
   int col = offset % screen_width();
