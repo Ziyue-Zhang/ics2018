067a70fbf35a5216f549741f7f6d81a5871363b7
tracer-ics2018 <tracer@njuics.org>
2018-12-03 22:39:30 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:39:30 up  2:53,  1 user,  load average: 0.19, 0.39, 0.41 7713e59d695ab9e2fe9cfacb4e932b58e7acbca

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -58,18 +58,18 @@ int fs_open(const char *pathname, int flags, int mode)
 	{
 		if(strcmp(file_table[i].name, pathname) == 0) 
 		{
-			Log("%s",pathname);
+			//Log("%s",pathname);
 			return i;
 		}
 	}
-	printf("%s\n",pathname);
+	Log("%s",pathname);
 	assert(0);
 	return -1;
 }
 
 ssize_t fs_read(int fd, void *buf, size_t len)
 {
-	Log("%d", fd);
+	//Log("%d", fd);
 	ssize_t fs_size = fs_filesz(fd);
 	ssize_t fs_offset = file_table[fd].open_offset;	 
 	if(fs_offset + len > fs_size)
@@ -88,7 +88,7 @@ ssize_t fs_read(int fd, void *buf, size_t len)
 
 ssize_t fs_write(int fd, const void *buf, size_t len)
 {			
-	Log("%d",fd);
+	//Log("%d",fd);
 	ssize_t fs_size = fs_filesz(fd);
 	ssize_t fs_offset = file_table[fd].open_offset;	
 	size_t n = len;
@@ -109,7 +109,7 @@ ssize_t fs_write(int fd, const void *buf, size_t len)
 
 off_t fs_lseek(int fd, off_t offset, int whence) 
 {
-  Log("%d",fd);
+  //Log("%d",fd);
   switch (whence) 
   {
     case SEEK_SET: file_table[fd].open_offset = offset; break;
