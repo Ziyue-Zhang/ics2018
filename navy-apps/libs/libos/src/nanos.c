b145b38dd4f76950c0003f2639210aa1b5ff520f
tracer-ics2018 <tracer@njuics.org>
2018-12-03 19:39:44 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:39:44 up  1:01,  1 user,  load average: 0.32, 0.37, 0.25 b779c44895c41a88854611db8a75a3d029cd01a8

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -23,8 +23,6 @@ intptr_t _syscall_(int type, intptr_t a0, intptr_t a1, intptr_t a2){
 #endif
 
 void _exit(int status) {
-if(status==6)
-	while(1);
   _syscall_(SYS_exit, status, 0, 0);
   while (1);
 }
