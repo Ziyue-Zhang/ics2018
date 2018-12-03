f291e9f8a4c567a167ebe0f975c06457a7d239ca
tracer-ics2018 <tracer@njuics.org>
2018-12-03 20:05:38 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:05:38 up  1:27,  1 user,  load average: 0.06, 0.22, 0.22 80dd5ec34217fb817eddbd8c5494a2b4981a258

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -38,6 +38,8 @@ static char dispinfo[128] __attribute__((used));
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   Log("%d %d",offset,len);
+  if(len>128)
+	len=128;
   memcpy(buf, dispinfo + offset, len);
 //memcpy(buf, dispinfo, len);
   return len;
