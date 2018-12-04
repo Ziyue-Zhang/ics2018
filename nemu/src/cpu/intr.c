ae49f81ae9d0a604cda318d83c0120110342dcad
tracer-ics2018 <tracer@njuics.org>
2018-12-04 10:26:30 +0800

    >  compile 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:26:30 up  3:48,  1 user,  load average: 0.62, 0.33, 0.24 d289bf34379848f9776670729cdcc2ca807b65eb

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -1,20 +1,21 @@
 #include "cpu/exec.h"
 #include "memory/mmu.h"
+#define NR_IRQ    256
+extern GateDesc idt[NR_IRQ];
 
 void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  uint32_t addr = cpu.idtr.base + NO*8;
-  uint32_t low = vaddr_read(addr,2);
-  uint32_t high = vaddr_read(addr+6,2);
-  uint32_t jp = (high << 16) | low;
-  cpu.eflags.val = 6;//(cpu.eflags.CF + 2 + (cpu.eflags.ZF << 6) + (cpu.eflags.SF << 7) + (cpu.eflags.IF << 9) + (cpu.eflags.OF << 11));
-  rtl_push(&cpu.eflags.val);
-  rtl_push(&cpu.CS);
-  rtl_push(&ret_addr);
-  rtl_j(jp);
- // cpu.eip = ret_addr;
+
+  //TODO();
+  rtl_push((rtlreg_t*)&cpu.eflags);
+  rtl_push((rtlreg_t*)&cpu.cs);
+  rtl_push((rtlreg_t*)&ret_addr); //this could be ret_addr
+  uint32_t base = cpu.idtr.base;
+  uint32_t offset;
+  offset = (vaddr_read(base + NO * 8, 4) & 0x0000ffff) | (vaddr_read(base + NO * 8 + 4, 4) & 0xffff0000);
+  rtl_j(offset);
 }
 
 void dev_raise_intr() {
