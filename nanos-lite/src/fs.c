ea72ef106130c3a5af159ab2c1587c7f51e5083c
tracer-ics2018 <tracer@njuics.org>
2018-12-09 01:01:25 +0800

    >  run 171860578 zhangziyue Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  01:01:25 up  5:46,  1 user,  load average: 0.07, 0.02, 0.00 1c98475a666f197e5bad27abf3caa1b0de69be47

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -58,7 +58,7 @@ int fs_open(const char *pathname, int flags, int mode)
 	{
 		if(strcmp(file_table[i].name, pathname) == 0) 
 		{
-			//Log("%s",pathname);
+			Log("%s",pathname);
             file_table[i].open_offset = 0;
 			return i;
 		}
